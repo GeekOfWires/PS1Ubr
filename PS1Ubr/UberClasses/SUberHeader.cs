db8bad403cdbaf4bca316b8a99ccf1052ec78e99
Mazo <mazo2@hotmail.com>
2018-10-17 13:48:17 +0100

    Cleanup SUberHeader, mark as unsafe and apply fixed byte length to FourCC

--- PS1Ubr/UberClasses/SUberHeader.cs
+++ PS1Ubr/UberClasses/SUberHeader.cs
@@ -1,32 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PS1Ubr
 {
-    /// <summary>
-    /// Literally just rewrote the SUberHeader as close as
-    /// possible
-    /// </summary>
-    public struct SUberHeader
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SUberHeader
     {
-        internal Byte[] FourCC;
-        internal UInt32 VersionA;
-        internal UInt32 VersionB;
-        internal UInt32 EntryCount;
-        internal UInt32 Vec3Count;
-        internal UInt32 Vec2Count;
-        internal UInt32 LookupCount;
-        internal UInt32 U32Count;
-        internal UInt32 MeshDataSize;
-        internal UInt32 Field_28;
+        internal fixed byte FourCC[4];
+        internal uint VersionA;
+        internal uint VersionB;
+        internal uint EntryCount;
+        internal uint Vec3Count;
+        internal uint Vec2Count;
+        internal uint LookupCount;
+        internal uint U32Count;
+        internal uint MeshDataSize;
+        internal uint Field_28;
 
-        internal static readonly Byte[] FourCCValue =
-            new Byte[4] { (byte)('u'), (byte)('b'), (byte)('e'), (byte)('r') };
+        internal static readonly byte[] FourCCValue = { (byte)'u', (byte)'b', (byte)'e', (byte)'r' };
 
-        internal static readonly UInt32 VersionAValue = 1;
-        internal static readonly UInt32 VersionBValue = 1;
+        internal static readonly uint VersionAValue = 1;
+        internal static readonly uint VersionBValue = 1;
     }
 }
+
+
