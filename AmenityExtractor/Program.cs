c0d3293e6f90dce6349a69b262d55f98ec07287c
Mazo <mazo2@hotmail.com>
2018-10-20 15:56:31 +0100

    Add placeholder AmenityExtractor project

---
+++ AmenityExtractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AmenityExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
