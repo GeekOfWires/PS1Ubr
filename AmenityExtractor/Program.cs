73f6445bd2bc4f481d27d97ecd095a0b886c3418
Mazo <mazo2@hotmail.com>
2018-10-22 13:03:58 +0100

    Add starting point for Amenity Extractor tool

--- AmenityExtractor/Program.cs
+++ AmenityExtractor/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using PS1Ubr;
+using System.Threading.Tasks;
 
 namespace AmenityExtractor
 {
@@ -6,7 +11,32 @@ namespace AmenityExtractor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var uberData = new List<CUberData>();
+            Parallel.ForEach(Directory.GetFiles("C:\\temp", "*.ubr"),
+                file => { uberData.Add(UBRReader.GetUbrData(file)); });
+
+            var entities = uberData.SelectMany(x => x.Entries).Select(x => x.Name).Distinct();
+        }
+
+        private static double PS1RotationToDegrees(int rotation)
+        {
+            // 16384 is the maximum rotation from the MPO files, which is equal to 360 or 0 degrees.
+            var incrementsPerDegree = 16384 / 360;
+
+            return rotation / incrementsPerDegree;
+        }
+
+        private static double DegreesToRadians(double degrees)
+        {
+            return (Math.PI / 180) * degrees;
+        }
+
+        private static (double, double) RotateXY(float x, float y, float radians)
+        {
+            var rotX = x * Math.Cos(radians) - y * Math.Sin(radians);
+            var rotY = y * Math.Cos(radians) + x * Math.Sin(radians);
+
+            return (rotX, rotY);
         }
     }
 }
